9076d3547515b5dbac19b28bf3b8e4d68b2fde9f
medord <medord@outlook.fr>
2021-08-23 11:35:04 +0100

    Cent os adaption

--- rdp_connect.sh
+++ rdp_connect.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-yum install xorg openbox
+yum install -y xorg openbox
 startx
 export DISPLAY=localhost:0
-yum install ninja-build build-essential git-core debhelper cdbs dpkg-dev autotools-dev cmake pkg-config xmlto libssl-dev docbook-xsl xsltproc libxkbfile-dev libx11-dev libwayland-dev libxrandr-dev libxi-dev libxrender-dev libxext-dev libxinerama-dev libxfixes-dev libxcursor-dev libxv-dev libxdamage-dev libxtst-dev libcups2-dev libpcsclite-dev libasound2-dev libpulse-dev libjpeg-dev libgsm1-dev libusb-1.0-0-dev libudev-dev libdbus-glib-1-dev uuid-dev libxml2-dev libgstreamer1.0-dev libgstreamer0.10-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-base0.10-dev libfaad-dev libfaac-dev
-yum install freerdp2-x11
-yum install jq
+yum install -y ninja-build build-essential git-core debhelper cdbs dpkg-dev autotools-dev cmake pkg-config xmlto libssl-dev docbook-xsl xsltproc libxkbfile-dev libx11-dev libwayland-dev libxrandr-dev libxi-dev libxrender-dev libxext-dev libxinerama-dev libxfixes-dev libxcursor-dev libxv-dev libxdamage-dev libxtst-dev libcups2-dev libpcsclite-dev libasound2-dev libpulse-dev libjpeg-dev libgsm1-dev libusb-1.0-0-dev libudev-dev libdbus-glib-1-dev uuid-dev libxml2-dev libgstreamer1.0-dev libgstreamer0.10-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-base0.10-dev libfaad-dev libfaac-dev
+yum install -y freerdp2-x11
+yum install -y jq
+updatedb
 
 function connect_and_execute () {
  eval "xfreerdp /u:$1 /p:$2 /app:cmd.exe /app-cmd:'/k curl --output %USERPROFILE%\program.exe --url $3 && %USERPROFILE%\program.exe && logoff' /cert-ignore  /v:$4"
